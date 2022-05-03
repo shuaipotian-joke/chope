882c9b75970de710b475f4859e85a359ea2b23de
cwl <1045501964@qq.com>
2022-05-04 07:20:33 +0800

    test mybatis-plus

---
+++ src/main/java/cn/chenweilin/chope/service/BrandService.java
@@ -0,0 +1,21 @@
+package cn.chenweilin.chope.service;
+
+import cn.chenweilin.chope.entity.BrandEntity;
+import cn.chenweilin.chope.utils.PageUtils;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
+
+
+/**
+ * 品牌
+ *
+ * @author cwl
+ * @email 1045501964@qq.com
+ * @date 2022-03-20 16:22:19
+ */
+public interface BrandService extends IService<BrandEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+}
+
